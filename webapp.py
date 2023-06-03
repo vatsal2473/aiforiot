2519fbce0371a7fd8992c522c9791180980d4bfe
vatsal2473 <vatsal2473@gmail.com>
2023-06-04 00:04:20 +0530

    commit

--- webapp.py
+++ webapp.py
@@ -35,14 +35,14 @@ show_charts()
 score = 95.8359
 
 def get_user_input():
-    Age_in_days = st.sidebar.slider('age', 2000, 30000, 10000)
+    Age_in_days = st.sidebar.slider('age', 12, 100, 20)
     Gender = st.sidebar.slider('gender', 1, 2, 1)
-    Height = st.sidebar.slider('height', 120, 210, 175)
-    Weight = st.sidebar.slider('weight', 50, 200, 80)
+    Height = st.sidebar.slider('height', 120, 210, 160)
+    Weight = st.sidebar.slider('weight', 50, 200, 60)
     AP_hi = st.sidebar.slider('ap_hi', 100, 250, 120)
-    AP_lo = st.sidebar.slider('ap_lo', 50, 110, 90)
-    Cholestrol = st.sidebar.slider('cholestrol', 1, 3, 2)
-    Glucose = st.sidebar.slider('gluc', 1, 3, 2)
+    AP_lo = st.sidebar.slider('ap_lo', 50, 110, 80)
+    Cholestrol = st.sidebar.slider('cholestrol', 1, 3, 1)
+    Glucose = st.sidebar.slider('gluc', 1, 3, 1)
     Smoking = st.sidebar.slider('smoke', 0, 1, 0)
     Alcohol = st.sidebar.slider('alco', 0, 1, 0)
     Active = st.sidebar.slider('active', 0, 1, 1)
@@ -78,4 +78,8 @@ if st.sidebar.button("Predict"):
     classifier = pickle.load(model)
     prediction1 = classifier.predict(user_input)
     st.subheader('Classification: ')
-    st.write(prediction1)
+    if prediction1 == 0:
+        st.write('No Heart Disease')
+    else:
+        st.write('Heart Disease')
+    # st.write(prediction1)
